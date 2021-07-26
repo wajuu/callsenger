07ebd075cd5bbf761bd8188cc40f84f6367aae28
wajuu <86391160+wajuu@users.noreply.github.com>
2021-07-26 18:31:00 +0900

    Update motion.js

--- js/motion.js
+++ js/motion.js
@@ -1,3 +1,6 @@
+
+//사이드메뉴
+
 $(document).ready(function(){
  
     $('.menu_btn>a').on('click', function(){
@@ -16,3 +19,38 @@ $(document).ready(function(){
     });
 
 });
+
+
+//로그인팝업
+
+$(document).ready(function(){
+
+    $('#login').click(function(){
+               
+    var popupX = (document.body.offsetWidth / 2) - (350 / 2);
+    //만들 팝업창 좌우 크기의 1/2 만큼 보정값으로 빼주었음
+
+    var popupY= (window.screen.height / 2) - (350 / 2);
+    //만들 팝업창 상하 크기의 1/2 만큼 보정값으로 빼주었음
+
+    window.open('login.html', '로그인', 'status=no, height=350, width=350,scrollbars=no left='+ popupX + ', top='+ popupY);
+});
+});
+
+
+//사이드고정배너
+
+$(document).ready(function()
+        {
+            $('#scroll3').click(function()
+            {
+                if($('#hidden-scroll').css('display') =='none')
+                {
+                    $('#hidden-scroll').css("display","block");
+                }
+                else
+                {
+                    $('#hidden-scroll').css("display","none");
+                }
+            });
+        });
